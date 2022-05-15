fde56727b77d6153a29933f4dc4de6ed267cbbee
EvertonMattos <devsukita@gmail.com>
2022-05-15 17:04:51 -0300

    adding functionality to the buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,16 +1,65 @@
+import { useState } from 'react';
 import Modal from 'react-modal'
+import imgClose from '../../assets/close.svg'
+import incomeIMG from '../../assets/income.svg'
+import outcomeIMG from '../../assets/outcome.svg'
+import { Container, TransactionsTypeButton, TypeBox } from './styles';
 
 interface NewTransactionModalProps{
   isOpen: boolean;
   onRequestClose: () =>void;
 }
 export function NewTransactionModal({isOpen,onRequestClose}:NewTransactionModalProps){
+  const [type,setType] = useState('deposit');
   return(
   <Modal
         isOpen={isOpen}
         onRequestClose={onRequestClose}
+        overlayClassName='react-modal-overlay'
+        className='react-modal-content'
       >
-        <h1>hellow</h1>
+        <button 
+        type='button'
+        onClick={onRequestClose}
+        className='react-modal-button'
+         >
+          <img src={imgClose} alt="Fechar Modal"  />
+        </button>
+       <Container>
+         <h2>Cadastrar Transação </h2>
+         <input placeholder="Titulo"        
+         />
+         <input
+         type="number"
+          placeholder="Valor"        
+         />
+         <TransactionsTypeButton>
+           <TypeBox 
+             onClick={()=>setType('deposit')}
+             isActive={type === 'deposit'}
+             >
+             <img src={incomeIMG} alt="Entradas"/>
+             <span>
+               Entrada
+             </span>
+           </TypeBox>
+           <TypeBox
+           onClick={()=>setType('withdraw')}
+           isActive={type === 'withdraw'}
+           >
+             <img src={outcomeIMG} alt="Saida"/>
+             <span>
+                Saida
+             </span>
+           </TypeBox>
+         </TransactionsTypeButton>
+         <input 
+         placeholder="Categoria"
+         />
+         <button type='submit'>
+           Cadastrar
+         </button>
+       </Container>
       </Modal>
   )
 }
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,75 @@
+import { darken } from 'polished';
 import styled  from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.form`
+h2{
+  color: var(--text-title);
+  font-size:1.5rem;
+  margin-bottom:2rem;
+}
+input{
+  width: 100%;
+  padding:0 1.5rem;
+  height: 4rem;
+  border-radius: 0.25rem;
+  border: 1px solid #d7d7d7;
+  background: #e7e9ee;
+  font-weight: 400;
+  font-size:1rem;
+  margin-top:1rem;
+}
+&::placeholder{
+  color: var(--text-body);
   
+}
+button[type="submit"]{
+  width: 100%;
+  padding:0 1.5rem;
+  height: 4rem;
+  background: var(--green);
+  color:#fff;
+  border-radius: 0.25rem;
+  border:0;
+  font-size: 1rem;
+  margin-top:1.5rem;
+  font-weight: 600;
+  transition: filter 0.5s ease-in;
+  &:hover{
+    filter:brightness(0.9);
+  }
+}
+`
+
+export const TransactionsTypeButton = styled.div`
+margin:1rem 0;
+display:grid;
+grid-template-columns: 1fr 1fr;
+gap: .5rem;
+
+`
+interface TypeBoxProps{
+  isActive: boolean;
+}
+export const TypeBox = styled.button<TypeBoxProps>`
+height:4rem;
+border:1px solid #d7d7d7;
+border-radius: .25rem;
+
+background: ${(props)=> props.isActive ? '#e44':'transparent'};
+display:flex;
+align-items:center;
+justify-content:center;
+&:hover{
+  border-color: ${darken(0.1, '#d7d7d7')} ;
+}
+img{
+  width:20px;
+  height:20px;
+}
+span{
+  display: inline-block;
+  margin-left:0.5rem;
+  color:var(--text-title);
+  font-size: 1rem;
+}
 `
